25bfaa0cb197fc2a44e9dff0d1feb87492a8009d
luiss07 <75475174+luiss07@users.noreply.github.com>
2021-12-13 16:30:49 +0100

    completed animals map

--- js/setFloraFauna.js
+++ js/setFloraFauna.js
@@ -2,7 +2,7 @@ setFloraFaunaPage = () => {
     document.getElementById("faunaFloraName").innerHTML = sessionStorage.getItem("selectedAnimalPlant");
     document.getElementById("ffParkName").innerHTML = sessionStorage.getItem("selectedPark");
     document.getElementById("ffDescription").innerHTML = sessionStorage.getItem("description");
-    if(sessionStorage.getItem("selectedPage") == "fauna"){
+    if (sessionStorage.getItem("selectedPage") == "fauna") {
         genMap();
     }
 }
@@ -11,29 +11,29 @@ setFloraFaunaPage = () => {
 
 async function setImageFloraFauna() {
     let sPage = sessionStorage.getItem("selectedPage");
-    let response = await fetch(variables.API_URL+sPage);
+    let response = await fetch(variables.API_URL + sPage);
     let data = await response.json();
     return data;
 }
 
-setImageFloraFauna().then(data=>{
+setImageFloraFauna().then(data => {
     let count = 0;
     let mainDiv = document.getElementById("ffImages");
-    data.forEach(elem =>{
-        if (elem.Tipo == sessionStorage.getItem("selectedAnimalPlant")){
-            elem.Immagine.forEach(image =>{
+    data.forEach(elem => {
+        if (elem.Tipo == sessionStorage.getItem("selectedAnimalPlant")) {
+            elem.Immagine.forEach(image => {
                 let div = document.createElement("div");
                 let img = document.createElement("img");
-                if (count == 0){    //different div & img element for first declaration
+                if (count == 0) {    //different div & img element for first declaration
                     div.setAttribute('class', 'carousel-item active');
-                    img.setAttribute('src', '../images/FloraFauna/'+image);
+                    img.setAttribute('src', '../images/FloraFauna/' + image);
                     img.setAttribute('class', 'myImg-fluid');
                     img.setAttribute('alt', sessionStorage.getItem("selectedPark") + 'image');
                     count++;
                     div.appendChild(img);
-                }else{
+                } else {
                     div.setAttribute('class', 'carousel-item');
-                    img.setAttribute('src', '../images/FloraFauna/'+image);
+                    img.setAttribute('src', '../images/FloraFauna/' + image);
                     img.setAttribute('class', 'd-block w-100');
                     img.setAttribute('alt', '...');
                     div.appendChild(img);
@@ -45,41 +45,39 @@ setImageFloraFauna().then(data=>{
 })
 
 // function to generate the selected animal Map
-
 genMap = () => {
-    
-    fetch(variables.API_URL + 'parco', {
+
+    fetch(variables.API_URL + 'parco/' + sessionStorage.getItem('selectedPark'), {
         method: 'GET'
     })
         .then(response => response.json())
-        .then(data => {
-            data.forEach(park => {
-                if (park.Parco == sessionStorage.getItem('selectedPark')) {
-                    ParseDMS(park.Posizione);
-                    let animalMap = setMapBox();
+        .then(park => {
+            ParseDMS(park[0].Posizione);
 
-                    fetch(variables.API_URL + 'sensoreGPS', {
-                        method: 'GET'
-                    })
-                        .then(response => response.json())
-                        .then(data => {
-                            data.forEach(sen => {
-                                if (sen.TipoAnimale == sessionStorage.getItem('selectedAnimalPlant') && sen.Parco == sessionStorage.getItem('selectedPark')) {
-                                    console.log(sen.SenId + " " + sen.Parco);
-                                    ParseDMS(sen.Posizione);
-                                    // marker creator
-                                    //let marker1 = new 
-                                    new mapboxgl.Marker()
-                                        .setLngLat([lng, lat])
-                                        .addTo(animalMap);
-                                }
-                            })
-                        });
-                }
+            fetch(variables.API_URL + 'sensoreGPS/' + sessionStorage.getItem('selectedAnimalPlant') + '/' + sessionStorage.getItem('selectedPark'), {
+                method: 'GET'
             })
+                .then(response => response.json())
+                .then(data => {
+                    if (isEmptyObject(data)) {
+                        let animalMap = setMapBox(); // init map
+                        data.forEach(sen => {
+                            ParseDMS(sen.Posizione);
+                            // marker creator
+                            //let marker1 = new 
+                            new mapboxgl.Marker()
+                                .setLngLat([lng, lat])
+                                .addTo(animalMap);
+
+                        })
+                    }else{
+                        console.log('Questo animale non viene tracciato.');
+                    }
+                });
         });
 }
 
+// create MapBox
 setMapBox = () => {
     // MapBox map
     mapboxgl.accessToken = 'pk.eyJ1IjoiZG9uZ2kiLCJhIjoiY2t4MGk4ZDN1MThpbzJvcDhpd294ZDAyMSJ9.dlbCTLS_xF3wQ1kD2HEIQw';
@@ -90,4 +88,9 @@ setMapBox = () => {
         center: [lng, lat]
     });
     return map;
+}
+
+// chech if fetch return empty json == animal is not tracked
+function isEmptyObject(obj) {
+    return Object.keys(obj).length;
 }
